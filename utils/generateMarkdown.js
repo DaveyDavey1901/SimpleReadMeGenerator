fa578c475b9f50584f705b9982642f9ab8533951
David Davies <David_Davies@mail.com>
2022-01-09 22:34:07 +0000

    added the license function and the markdown data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,50 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderlicenseBadge(data) {
+  const TypeOfLicense = data.license[0];
+  let licenseBadge = " ";
+  if (TypeOfLicense === "MIT") {
+    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  }
+  if (TypeOfLicense === "GNU General Public License v3") {
+    licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  }
+  if (TypeOfLicense === "Apache 2.0 License") {
+    licenseBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  }
+  if (TypeOfLicense === "Mozilla Public License 2.0") {
+    licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  }
+  
+  return licenseBadge;
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+	return `# ${data.title}
+## Table of Contents:
+  1. [Description](#description) 
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)  
+  4. [License](#License)
+  5. [Contributing](#Contributing)
+  6. [Tests](#Tests)
+  7. [GitHub](#GitHub)
+  8. [E-mail](#E-mail)
+## Description
+${data.description} 
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contributing}
+## repository
+${data.repository}
+## License
+${renderlicenseBadge(data)} Click Badge for License Details.
+## Questions:
+## GitHub
+${data.github}
+## E-mail
+Drop me an enail at ${data.email} if you would like to know more or would like to contribute.` 
 }
-
 module.exports = generateMarkdown;
